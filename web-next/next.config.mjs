59668610be01bbfd14baf6649f8901c0eebfbe7f
Anhzf <me@anhzf.dev>
2024-08-06 22:25:26 +0700

    fix(web-next): :bug: Set `images.remotePatterns`

--- web-next/next.config.mjs
+++ web-next/next.config.mjs
@@ -1,15 +1,14 @@
 import { withPayload } from '@payloadcms/next/withPayload';
 
+console.log('process.env.APP_SCHEME', process.env.APP_SCHEME, process.env.APP_HOSTNAME, process.env.APP_PORT, process.env.APP_URL);
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     dangerouslyAllowSVG: true,
     remotePatterns: [
-      {
-        protocol: process.env.APP_SCHEME,
-        hostname: process.env.APP_HOSTNAME,
-        port: process.env.APP_PORT,
-      }
+      { hostname: process.env.APP_HOSTNAME },
     ],
   },
 };
